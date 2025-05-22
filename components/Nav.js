c88a4e230db6b3e461355fddc77ff1c8d17360ba
Pouya Hendy <pouya.hedy@gmail.com>
2025-05-22 18:18:08 +0330

    start project + added navbar

---
+++ components/Nav.js
@@ -0,0 +1,73 @@
+// icons
+import {
+  HiHome,
+  HiUser,
+  HiViewColumns,
+  HiRectangleGroup,
+  HiChatBubbleBottomCenterText,
+  HiEnvelope,
+} from "react-icons/hi2";
+
+//next link
+import Link from "next/link";
+
+//next router
+import { useRouter } from "next/router";
+
+// nav data
+export const navData = [
+  { name: "home", path: "/", icon: <HiHome /> },
+  { name: "about", path: "/about", icon: <HiUser /> },
+  { name: "services", path: "/services", icon: <HiRectangleGroup /> },
+  { name: "work", path: "/work", icon: <HiViewColumns /> },
+  {
+    name: "testimonials",
+    path: "/testimonials",
+    icon: <HiChatBubbleBottomCenterText />,
+  },
+  {
+    name: "contact",
+    path: "/contact",
+    icon: <HiEnvelope />,
+  },
+];
+
+const Nav = () => {
+  const router = useRouter();
+  const pathname = router.pathname;
+
+  return (
+    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
+      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px]  xl:h-max py-8 bg-white/10 backdrop-blur-sm text-white text-3xl xl:text-xl xl:rounded-full">
+        {navData.map((link, index) => {
+          return (
+            <Link
+              className={`${
+                link.path === pathname && "text-cyan-500"
+              } relative flex items-center group hover:text-cyan-500 justify-center xl:justify-center py-3 xl:py-0 transition-all duration-400`}
+              href={link.path}
+              key={index}
+            >
+              {/* name */}
+              <div className="absolute pr-14 right-0 hidden xl:group-hover:flex ">
+                <div className="bg-gradient-to-r from-cyan-200 to-white relative flex text-primary items-center p-[6px] rounded-[7px]">
+                  <div className="text-[12px] leading-none font-semibold capitalize">
+                    {link.name}
+                  </div>
+                  <div className="border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2 top-[50%] -translate-y-1/2"></div>
+                </div>
+              </div>
+
+              {/* icon */}
+              <div className="relative flex items-center justify-center">
+                {link.icon}
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
