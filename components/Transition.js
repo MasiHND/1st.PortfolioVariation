41d1e294de02d94ae97e3879b84e3d65d82ed307
Pouya Hendy <pouya.hedy@gmail.com>
2025-05-22 19:04:18 +0330

    added laoding

--- components/Transition.js
+++ components/Transition.js
@@ -1,7 +1,54 @@
+//framer motion
+import { motion } from "framer-motion";
+
+//Next Image
+import Image from "next/image";
+
+//variants
+const transitionVariants = {
+  initial: {
+    x: "100%",
+    width: "100%",
+  },
+  animate: {
+    x: "0%",
+    width: "0%",
+  },
+  exit: {
+    x: ["0%", "100%"],
+    width: ["0%", "100%"],
+  },
+};
+
 const Transition = () => {
   return (
     <>
-      <div>Transition</div>
+      <motion.div
+        className="flex fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-indigo-950 items-center justify-center"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+      >
+        <Image src="/MasiHND-LogoBigDark.png" width={500} height={500} alt="" />
+      </motion.div>
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-violet-950/90"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
+      ></motion.div>
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-purple-950/80"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
+      ></motion.div>
     </>
   );
 };
