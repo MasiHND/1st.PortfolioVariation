17defbe8eadf757f6ddc7f880c867106e4b11c6f
Pouya Hendy <pouya.hedy@gmail.com>
2025-05-23 18:51:09 +0330

    added header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,41 @@
+// next image
+import Image from "next/image";
+
+//next Link
+import Link from "next/link";
+
+//components
+import Socials from "../components/Socials";
+
 const Header = () => {
-  return <header className="absolute z-30 w-full">Header</header>;
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[150px]">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
+          {/* Logo */}
+          <Link href={"/"}>
+            <Image
+              src={"/pouyaLogo.png"}
+              width={220}
+              height={48}
+              alt="Pouya-Logo"
+              priority={true}
+            />
+          </Link>
+
+          <div className="justify-center items-center gap-8 hidden lg:flex">
+            <Socials />
+            <Image
+              src={"/MasiHND-Logo1.png"}
+              width={150}
+              height={150}
+              alt="MasiHND-Logo"
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,54 @@
+// links
+import Link from "next/link";
+
+// icons
+import {
+  RiTelegramLine,
+  RiWhatsappLine,
+  RiGithubLine,
+  RiLinkedinFill,
+} from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-4 text-2xl">
+      <Link
+        href={"https://t.me/JesusHND"}
+        rel="noopener noreferrer"
+        target="_blank"
+        className="hover:text-accent transition-all duration-500 "
+      >
+        <RiTelegramLine />
+      </Link>
+
+      <Link
+        href={"http://Wa.me/+989120597807"}
+        rel="noopener noreferrer"
+        target="_blank"
+        className="hover:text-accent transition-all duration-500 "
+      >
+        <RiWhatsappLine />
+      </Link>
+
+      <Link
+        href={"https://github.com/MasiHND"}
+        rel="noopener noreferrer"
+        target="_blank"
+        className="hover:text-accent transition-all duration-500 "
+      >
+        <RiGithubLine />
+      </Link>
+
+      <Link
+        href={"https://www.linkedin.com/in/pouya-hendy/"}
+        rel="noopener noreferrer"
+        target="_blank"
+        className="hover:text-accent transition-all duration-500 "
+      >
+        <RiLinkedinFill />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
