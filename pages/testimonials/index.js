30b2fd6b7eb56dd9f7982c0f91ef1897e2514fa0
Pouya Hendy <pouya.hedy@gmail.com>
2025-05-28 19:36:45 +0330

    testomonial page added

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,5 +1,36 @@
+// framer motion
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
+// components
+import TestimonialSlider from "../../components/TestimonialSlider";
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center">
+      <div className="flex flex-col justify-center container mx-auto h-full">
+        {/* title */}
+        <motion.h2
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="h2 capitalize mb-12 xl:mb-0"
+        >
+          What clients <sapn className="text-accent">say.</sapn>
+        </motion.h2>
+        {/* slider */}
+        <motion.div
+          variants={fadeIn("down", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+        >
+          <TestimonialSlider />
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
